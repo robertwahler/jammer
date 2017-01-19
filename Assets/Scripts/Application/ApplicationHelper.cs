268d4aa9ed192ee822a687b086485faa266dfcc5
Robert Wahler <robert@gearheadforhire.com>
2017-01-19 14:23:41 -0500

    ApplicationHelper.Version cleanup

--- Assets/Scripts/Application/ApplicationHelper.cs
+++ Assets/Scripts/Application/ApplicationHelper.cs
@@ -55,28 +55,30 @@ namespace Jammer {
     /// </summary>
     public static string AppAssets = System.IO.Path.Combine(AppPath, "Assets/Resources");
 
-
+    /// <summary>
+    /// Application version deserialized from a JSON resource 'version.txt'
+    /// </summary>
     public static Version Version {
       get {
         if (version == null) {
           try {
-          TextAsset textData = (TextAsset) Resources.Load("Version", typeof(TextAsset));
-          if (textData != null) {
-            version = JsonConvert.DeserializeObject<Version>(textData.text);
-          }
-          else {
-            Log.Error(string.Format("GameManager.Version failed to load Version.txt from resources"));
-          }
-
+            TextAsset textData = (TextAsset) Resources.Load("Version", typeof(TextAsset));
+            if (textData != null) {
+              version = JsonConvert.DeserializeObject<Version>(textData.text);
+            }
+            else {
+              Log.Error(string.Format("ApplicationHelper.Version failed to load Version.txt from resources"));
+            }
           }
           catch (System.Exception e) {
-            Log.Error(string.Format("GameManager.Version failed with {0}", e.ToString()));
+            Log.Error(string.Format("ApplicationHelper.Version failed with {0}", e.ToString()));
           }
         }
         return version;
       }
     }
     private static Version version;
+
   }
 }
 
