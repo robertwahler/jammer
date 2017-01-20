8e83a2499989762dcd8379f43bcbe0383568b4c6
Robert Wahler <robert@gearheadforhire.com>
2017-01-20 09:59:32 -0500

    rename dontdestroy behaviour
    Less verbose

--- Assets/Scripts/Behaviours/DontDestroy.cs
+++ Assets/Scripts/Behaviours/DontDestroy.cs
@@ -5,7 +5,11 @@ using System.Linq;
 
 namespace Jammer.Behaviours {
 
-  public class DontDestroyOnLoad : BehaviourBase {
+  /// <summary>
+  /// Attach to gameObject to prevent the entire hierarchy from being destroyed
+  /// when a new scene is loaded.
+  /// </summary>
+  public class DontDestroy : BehaviourBase {
 
     void Awake() {
       DontDestroyOnLoad(transform.gameObject);
