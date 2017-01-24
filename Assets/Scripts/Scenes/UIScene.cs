c9d383c4bf3f2f1b370b3aec599ecce399cb995f
Robert Wahler <robert@gearheadforhire.com>
2017-01-24 12:49:30 -0500

    UI scene allows for specifying a design time scene
    To speed menu development when pressing the start button in the IDE

--- Assets/Scripts/Scenes/UIScene.cs
+++ Assets/Scripts/Scenes/UIScene.cs
@@ -13,6 +13,13 @@ namespace Jammer.Scenes {
   /// </summary>
   public class UIScene : BaseScene {
 
+    /// <summary>
+    /// Show on start in the Unity editor if this scene is the active scene.
+    /// Assign ref in IDE.
+    /// </summary>
+    [Header("Design Time Only")]
+    public Menu designTimeMenu;
+
     protected override void OnEnable() {
       Log.Debug(string.Format("UIScene.OnEnable()"));
       base.OnEnable();
@@ -23,15 +30,24 @@ namespace Jammer.Scenes {
       }
     }
 
-    protected void Start() {
-      Log.Debug(string.Format("UIScene.Start()"));
+    // show menu automatically if this is the editor scene
+    #if UNITY_EDITOR
+      protected void Start() {
+        Log.Debug(string.Format("UIScene.Start()"));
 
-      if (ActiveScene.name == ApplicationConstants.UIScene) {
-        // we are design time editing, show the menus
-        Events.Raise(new MenuCommandEvent() { Handled=false, MenuId=MenuId.Main, State=MenuState.Open });
+        MenuId id;
+
+        if (ActiveScene.name == ApplicationConstants.UIScene) {
+          if (designTimeMenu != null) {
+            id = designTimeMenu.Id;
+          }
+          else {
+            id = MenuId.Main;
+          }
+          Events.Raise(new MenuCommandEvent() { Handled=false, MenuId=id, State=MenuState.Open });
+        }
       }
-
-    }
+    #endif
 
   }
 }
