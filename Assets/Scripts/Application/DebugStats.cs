d05f21836d79eef26f20032d7cdc0328279404e6
Robert Wahler <robert@gearheadforhire.com>
2017-01-28 14:17:35 -0500

    DebugStats.cs turns of debug text if compiler define not set

--- Assets/Scripts/Application/DebugStats.cs
+++ Assets/Scripts/Application/DebugStats.cs
@@ -43,7 +43,7 @@ namespace Jammer {
         StartCoroutine(CalculateStats());
         StartCoroutine(UpdateDisplay());
       #else
-        enabled = false;
+        gameObject.SetActive(false);
         yield break;
       #endif
     }
