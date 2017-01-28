f9bb6c0ec0113dbd5b85dbf2c05a6180de20abbb
Robert Wahler <robert@gearheadforhire.com>
2017-01-28 15:34:28 -0500

    don't hard-code ProductCode, make all application constants readonly

--- Assets/Scripts/Application/ApplicationConstants.cs
+++ Assets/Scripts/Application/ApplicationConstants.cs
@@ -8,22 +8,22 @@ namespace Jammer {
     /// Application product name. Used as namespace. Application exe name. Save
     /// folder name. etc.
     /// </summary>
-    public const string ProductCode = "Jammer";
+    public static readonly string ProductCode = Application.productName;
 
     /// <summary>
     /// Scene to load after splash shown
     /// </summary>
-    public const string StartScene = "Start";
+    public static readonly string StartScene = "Start";
 
     /// <summary>
     /// Main game play scene
     /// </summary>
-    public const string MainScene = "Level1";
+    public static readonly string MainScene = "Level1";
 
     /// <summary>
     /// Scene that contains the menus
     /// </summary>
-    public const string UIScene = "UI";
+    public static readonly string UIScene = "UI";
 
   }
 }
