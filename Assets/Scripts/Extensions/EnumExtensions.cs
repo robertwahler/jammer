447a2aa04e9a14357be3634083dad1264e7204e3
Robert Wahler <robert@gearheadforhire.com>
2017-01-28 08:00:20 -0500

    add EnumExtensions.cs
    For bit flag checking

---
+++ Assets/Scripts/Extensions/EnumExtensions.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jammer.Extensions {
+
+  public static class EnumExtension {
+
+    /// <summary>
+    /// Check to see if a flags enumeration has a specific flag set.
+    /// </summary>
+    /// <param name="variable">Flags enumeration to check</param>
+    /// <param name="value">Flag to check for</param>
+    public static bool HasFlag(this Enum variable, Enum value) {
+      if (variable == null)
+        return false;
+
+      if (value == null)
+        throw new ArgumentNullException("value");
+
+      // Not as good as the .NET 4 version of this function, but should be good enough
+      if (!Enum.IsDefined(variable.GetType(), value)) {
+        throw new ArgumentException(string.Format( "Enumeration type mismatch.  The flag is of type '{0}', was expecting '{1}'.", value.GetType(), variable.GetType()));
+      }
+
+      ulong num = Convert.ToUInt64(value);
+      return ((Convert.ToUInt64(variable) & num) == num);
+    }
+
+  }
+}
